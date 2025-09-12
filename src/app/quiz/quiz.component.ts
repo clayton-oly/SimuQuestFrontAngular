1f3f9d8af983f10d67025b0a7b0c4c3fa40487ed
Clayton Rocha <clayton.will08@gmail.com>
2025-09-12 16:56:36 -0300

    Criação do footer, add botões de proximo e anterior, organização dos componentes

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Question } from '../models/question.model';
 import { QuizService } from '../core/services/quiz.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-quiz',
-  imports: [[NgFor], [FormsModule]],
+  imports: [[NgFor], [FormsModule], [NgIf]],
   templateUrl: './quiz.component.html',
   styleUrl: './quiz.component.css'
 })
@@ -23,8 +23,23 @@ export class QuizComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadQuestions();
+    this.diminuirSegundo()
   }
 
+  tempoRestante = 3600; // em segundos
+
+  formatarTempo(segundos: number): string {
+    const min = Math.floor(segundos / 60);
+    const seg = segundos % 60;
+    // Adiciona zero à esquerda se for < 10
+    return `${min.toString().padStart(2, '0')}:${seg.toString().padStart(2, '0')}`;
+  }
+
+  diminuirSegundo() {
+    setInterval(() => {
+      this.tempoRestante--;
+    }, 1000);
+  }
 
   loadQuestions() {
     this.quizService.getQuestions().subscribe({
@@ -34,5 +49,19 @@ export class QuizComponent implements OnInit {
       },
       error: (err) => console.error('Erro ao carregar questões:', err)
     });
+  }
+
+  currentIndex = 0;
+
+  proxima() {
+    if (this.currentIndex < this.questions.length - 1) {
+      this.currentIndex++;
+    }
+  }
+
+  anterior() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
   }
 }
