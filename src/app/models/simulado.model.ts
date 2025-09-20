021966af10d33d54ff4327caf65cba267769e773
Clayton Rocha <clayton.will08@gmail.com>
2025-09-20 03:52:19 -0300

    Alteracao na cor do navbar e footer, Add imagem do exame

--- src/app/models/simulado.model.ts
+++ src/app/models/simulado.model.ts
@@ -3,6 +3,7 @@ import { Question } from './question.model';
 export interface SimulatedExam {
   id: number;
   nome: string;
+  imagem: string;
   descricao?: string;
   dataCriacao: string;
   questions: Question[];
