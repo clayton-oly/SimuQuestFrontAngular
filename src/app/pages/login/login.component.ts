2d7ec20a35e4b9dc6933f5aa3555f09c71af816f
Clayton Rocha <clayton.will08@gmail.com>
2025-09-20 23:56:21 -0300

    Add Login Component e Configuracao do metodo de logar

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { LoginService } from '../../core/services/login.service';
+import { Login } from '../../models/login';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  login: Login = {
+    email: '',
+    senha: '',
+    id: 0
+  };
+
+  constructor(private loginService: LoginService) { }
+
+  logar() {
+    this.loginService.login(this.login).subscribe({
+      next: (res) => console.log('Login bem-sucedido', res),
+      error: (err) => console.error('Erro no login', err)
+    });
+  }
+}
