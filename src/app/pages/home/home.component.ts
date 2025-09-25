89b970543f7f0841347c8e05c5b425e5a8c2cadb
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-09-24 22:20:09 -0300

    Criações das seasons na component quiz: -questao-card -timer-question -text-question -question-alternatives -alerta-question-alternative -navigation -finish
    Importação da biblioteca MatIconModule

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,34 +1,33 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router'; // caso queira links
+import { RouterModule } from '@angular/router';
 import { Router } from '@angular/router';
 import { SimuladoService } from '../../core/services/simulado.service';
 import { SimulatedExam } from '../../models/simulado.model';
+import { MatIconModule } from '@angular/material/icon'; // importa o módulo de ícones
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatIconModule], //adiciona MatIconModule aqui
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'] // tem que ser "styleUrls" no plural
 })
-
 export class HomeComponent {
   simulados: SimulatedExam[] = [];
+
+  constructor(private simuladoService: SimuladoService, private router: Router) {}
 
   ngOnInit(): void {
     this.loadExams();
   }
 
-
-  constructor(private simuladoService: SimuladoService, private router: Router) { }
-
   login() {
     console.log('Botão de login clicado');
   }
 
-  iniciar(id:number) {
-    console.log(id)
-    this.router.navigate(['/simulado', id])
+  iniciar(id: number) {
+    console.log(id);
+    this.router.navigate(['/simulado', id]);
   }
 
   loadExams() {
