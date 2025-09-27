76cc77386a840ecfe75d875e475f2f0b3f84abc2
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-09-26 21:21:33 -0300

    alteração na estilização da home

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideHttpClient(),
